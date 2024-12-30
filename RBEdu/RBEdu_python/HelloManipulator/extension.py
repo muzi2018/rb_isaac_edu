f6ca264e7ecb6da73d83a18503ed88c7688f00ab
kimsooyoung <tge1375@naver.com>
2024-12-30 01:18:19 -0800

    Till manipulator ex

--- RBEdu/RBEdu_python/HelloManipulator/extension.py
+++ RBEdu/RBEdu_python/HelloManipulator/extension.py
@@ -16,9 +16,9 @@
 import os
 
 from omni.isaac.examples.base_sample import BaseSampleExtension
-from .hello_manip_1 import HelloManip
+# from .hello_manip_1 import HelloManip
 # from .hello_manip_2 import HelloManip
-# from .hello_manip_3 import HelloManip
+from .hello_manip_3 import HelloManip
 # from .hello_manip_4 import HelloManip
 # from .hello_manip_5 import HelloManip
 
@@ -46,8 +46,8 @@ class Extension(BaseSampleExtension):
         super().start_extension(
             menu_name="RoadBalanceEdu",
             submenu_name="",
-            name="HelloManipulator",
-            title="HelloManipulator",
+            name="Example9 - HelloManipulator",
+            title="Example9 - HelloManipulator",
             doc_link="https://docs.omniverse.nvidia.com/isaacsim/latest/core_api_tutorials/tutorial_core_hello_world.html",
             overview="This Example introduces the user on how to do cool stuff with Isaac Sim through scripting in asynchronous mode.",
             file_path=os.path.abspath(__file__),
