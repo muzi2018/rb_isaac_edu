545139bf158b446793fe9154bd9b4fbc0f745f4a
kimsooyoung <tge1375@naver.com>
2025-04-25 01:07:05 -0700

    direct rl added

--- RBEduIsaacLab/robots/__init__.py
+++ RBEduIsaacLab/robots/__init__.py
@@ -1,18 +1,3 @@
-# from .simulation import Simulator
-# from .pendulum_plant import PendulumPlant
-# from .gym_environment import SimplePendulumEnv
-
-# from gymnasium.envs.registration import register
-
-# register(
-#     id="gymnasium_env/SimplePendulum-v0",
-#     entry_point=SimplePendulumEnv,
-#     kwargs={
-#         "simulator": None
-#     }
-# )
-
-
 import gymnasium as gym
 
 ##
@@ -25,5 +10,14 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.pendulum_manager_based_env:CartpoleRLEnvCfg",
+    },
+)
+
+gym.register(
+    id="RoadBalance-Pendulum-Direct-v0",
+    entry_point=f"{__name__}.pendulum_direct_rl_env:CartpoleDirectRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.pendulum_direct_rl_env:CartpoleEnvCfg",
     },
 )
---
+++ RBEduIsaacLab/robots/pendulum_direct_rl_env.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import math
+import torch
+from collections.abc import Sequence
+
+# from isaaclab_assets.robots.cartpole import CARTPOLE_CFG
+from .pendulum import PENDULUM_CFG
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import Articulation, ArticulationCfg
+from isaaclab.envs import DirectRLEnv, DirectRLEnvCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sim import SimulationCfg
+from isaaclab.sim.spawners.from_files import GroundPlaneCfg, spawn_ground_plane
+from isaaclab.utils import configclass
+from isaaclab.utils.math import sample_uniform
+
+
+@configclass
+class CartpoleEnvCfg(DirectRLEnvCfg):
+    # env
+    decimation = 2
+    episode_length_s = 5.0
+    action_scale = 100.0  # [N]
+    action_space = 1
+    observation_space = 4
+    state_space = 0
+
+    # simulation
+    sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
+
+    # robot
+    robot_cfg: ArticulationCfg = PENDULUM_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    pole_dof_name = "continuous_joint"
+
+    # scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
+
+    # reset
+    initial_pole_angle_range = [-math.pi/4, math.pi/4]  # the range in which the pole angle is sampled from on reset [rad]
+
+    # reward scales
+    rew_scale_alive = 1.0
+    rew_scale_terminated = -2.0
+    rew_scale_pole_pos = -1.0
+    rew_scale_cart_vel = -0.01
+    rew_scale_pole_vel = -0.005
+
+
+class CartpoleDirectRLEnv(DirectRLEnv):
+    cfg: CartpoleEnvCfg
+
+    def __init__(self, cfg: CartpoleEnvCfg, render_mode: str | None = None, **kwargs):
+        super().__init__(cfg, render_mode, **kwargs)
+
+        self._pole_dof_idx, _ = self._robot.find_joints(self.cfg.pole_dof_name)
+        self.action_scale = self.cfg.action_scale
+
+        self.joint_pos = self._robot.data.joint_pos
+        self.joint_vel = self._robot.data.joint_vel
+
+    def _setup_scene(self):
+        self._robot = Articulation(self.cfg.robot_cfg)
+        # add ground plane
+        spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
+        # clone and replicate
+        self.scene.clone_environments(copy_from_source=False)
+        # add articulation to scene
+        self.scene.articulations["cartpole"] = self._robot
+        # add lights
+        light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
+        light_cfg.func("/World/Light", light_cfg)
+
+    def _pre_physics_step(self, actions: torch.Tensor) -> None:
+        self.actions = self.action_scale * actions.clone()
+
+    def _apply_action(self) -> None:
+        self._robot.set_joint_effort_target(self.actions, joint_ids=self._pole_dof_idx)
+
+    def _get_observations(self) -> dict:
+        obs = torch.cat(
+            (
+                self.joint_pos[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
+                self.joint_vel[:, self._pole_dof_idx[0]].unsqueeze(dim=1),
+            ),
+            dim=-1,
+        )
+        observations = {"policy": obs}
+        return observations
+
+    def _get_rewards(self) -> torch.Tensor:
+        total_reward = compute_rewards(
+            self.cfg.rew_scale_alive,
+            self.cfg.rew_scale_terminated,
+            self.cfg.rew_scale_pole_pos,
+            self.cfg.rew_scale_pole_vel,
+            self.joint_pos[:, self._pole_dof_idx[0]],
+            self.joint_vel[:, self._pole_dof_idx[0]],
+            self.reset_terminated,
+        )
+        return total_reward
+
+    def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
+        self.joint_pos = self._robot.data.joint_pos
+        self.joint_vel = self._robot.data.joint_vel
+
+        time_out = self.episode_length_buf >= self.max_episode_length - 1
+        # out_of_bounds = torch.any(torch.abs(self.joint_pos[:, self._cart_dof_idx]) > self.cfg.max_cart_pos, dim=1)
+        
+        # Rotate over 2 pi => out of bounds
+        out_of_bounds = torch.any(torch.abs(self.joint_pos[:, self._pole_dof_idx]) > math.pi * 2, dim=1)
+        return out_of_bounds, time_out
+
+    def _reset_idx(self, env_ids: Sequence[int] | None):
+        if env_ids is None:
+            env_ids = self._robot._ALL_INDICES
+        super()._reset_idx(env_ids)
+
+        joint_pos = self._robot.data.default_joint_pos[env_ids]
+        joint_pos[:, self._pole_dof_idx] += sample_uniform(
+            self.cfg.initial_pole_angle_range[0] * math.pi,
+            self.cfg.initial_pole_angle_range[1] * math.pi,
+            joint_pos[:, self._pole_dof_idx].shape,
+            joint_pos.device,
+        )
+        joint_vel = self._robot.data.default_joint_vel[env_ids]
+
+        default_root_state = self._robot.data.default_root_state[env_ids]
+        default_root_state[:, :3] += self.scene.env_origins[env_ids]
+
+        self.joint_pos[env_ids] = joint_pos
+        self.joint_vel[env_ids] = joint_vel
+
+        self._robot.write_root_pose_to_sim(default_root_state[:, :7], env_ids)
+        self._robot.write_root_velocity_to_sim(default_root_state[:, 7:], env_ids)
+        self._robot.write_joint_state_to_sim(joint_pos, joint_vel, None, env_ids)
+
+
+@torch.jit.script
+def compute_rewards(
+    rew_scale_alive: float,
+    rew_scale_terminated: float,
+    rew_scale_pole_pos: float,
+    rew_scale_pole_vel: float,
+    pole_pos: torch.Tensor,
+    pole_vel: torch.Tensor,
+    reset_terminated: torch.Tensor,
+):
+    rew_alive = rew_scale_alive * (1.0 - reset_terminated.float())
+    rew_termination = rew_scale_terminated * reset_terminated.float()
+    rew_pole_pos = rew_scale_pole_pos * torch.sum(torch.square(pole_pos).unsqueeze(dim=1), dim=-1)
+    rew_pole_vel = rew_scale_pole_vel * torch.sum(torch.abs(pole_vel).unsqueeze(dim=1), dim=-1)
+    total_reward = rew_alive + rew_termination + rew_pole_pos + rew_pole_vel
+    return total_reward
